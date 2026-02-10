1185aa93a9482672c1d82f414e9de8a4b61ac16b
Pablo12612 <240701@utxicotepec.edu.mx>
2026-02-10 17:27:33 -0600

    Api inicio

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,7 +1,9 @@
+import {APIProvider,Map, AdvancedMarker, Pin} from '@vis.gl/react-google-maps';
 import History from './assets/History.jpg';
 import './Card.css';
 
 function Card() {
+    const position = {lat:20.276439346881805, Ing:-97.9636377050528};
     return (
         <div 
             className="card-con-fondo"
@@ -10,6 +12,8 @@ function Card() {
             <div className="card-contenido">
                 <h2>Vive la Historia del Wrestling Profesional</h2>
                 <p>Descubre los momentos más icónicos del mundo del lucha profesional.</p>
+
+
             </div>
         </div>
     );
